075487229a42fefafdb3f60ab5bba9efb67da2e0
Cwynar <80697570+Cwynar@users.noreply.github.com>
2022-10-27 18:42:53 +0200

    Add files via upload

---
+++ lab3cw.py
@@ -0,0 +1,44 @@
+'''
+print("...:ZADANIE 1:...")
+x = int(input("Podaj 1. liczbe: "))
+y = int(input("Podaj 2. liczbe: "))
+
+for liczba in range(x,y):
+    print(liczba, end=' ')
+'''
+'''
+
+print("...:ZADANIE 2:...")
+
+x = -4
+while (x<=4):
+    y=2*x**2-5*x-8
+    print(f'f({x}) = {y}')
+    x+=0.5
+'''
+'''
+print("...:ZADANIE 3:...")
+liczba = 0
+while liczba > 0:
+    liczba = int(input("Podaj liczbę: "))
+'''
+
+'''
+print("...:ZADANIE 4:...")
+x = int(input("Podaj 1. liczbe: "))
+y = int(input("Podaj 2. liczbe: "))
+if(x>y):
+    x,y=y,x
+
+while (x<=y):
+    if x%2 == 1:
+        x = x + 1
+        continue
+
+    print(x, end='')
+    x = x + 1
+'''
+'''
+print("...:ZADANIE 5:...")
+n = int(input("Pdaj licze studentów: "))
+'''
